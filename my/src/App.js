006367d6724cc9efcb3d7f7022ffb0c5ae8a85b1
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-04 12:04:16 -0700

    doing

---
+++ my/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <><nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>contact us</li>
+    </nav>
+    </>
+  );
+}
+
+export default App;
